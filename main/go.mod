bc4c9eccff5b073859f324f80c2c3b485a0c98f4
amit <amit@inceptioncloud.io>
2021-06-12 17:06:52 +0530

    1000 go routines doing curl on external sites

---
+++ main/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.15
+
+require github.com/gocql/gocql v0.0.0-20190208221138-c53c3654dc8a
---
+++ main/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+
+func main(){
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	defer wg.Wait()
+
+	for i:=1; i <1000; i++ {
+		go test()
+	}
+}
+
+func test() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	//fmt.Println(<-c)
+
+	// Doing a for loop over a channel will keep waiting for the new data to show up in the channel
+	// which it will retrieve in l
+	for l := range c {
+		// its a function literal which is similar to lamda in java
+		go func(link string) {
+			//time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
